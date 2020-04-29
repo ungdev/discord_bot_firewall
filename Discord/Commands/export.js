03469c68cf6d98ed068177d1657c052beb87a196
larueli <ivann.laruelle@gmail.com>
2020-04-29 21:58:53 +0200

    add security to env Var

--- Discord/Commands/export.js
+++ Discord/Commands/export.js
@@ -5,6 +5,15 @@ let path = require("path");
 
 module.exports = async function (/** module:"discord.js".Message */ msg) {
   if (
+    typeof process.env.DISCORD_CHAT_EXPORT_PATH === "undefined" ||
+    typeof process.env.DISCORD_CHAT_EXPORTER_EXE_PATH === "undefined" ||
+    process.env.DISCORD_CHAT_EXPORT_PATH === "" ||
+    process.env.DISCORD_CHAT_EXPORTER_EXE_PATH === ""
+  ) {
+    msg.reply(
+      " :warning: L'exécution actuelle ne prend pas en charge l'export."
+    );
+  } else if (
     (await msg.member.fetch()).roles.highest.comparePositionTo(
       process.env.ROLE_ENSEIGNANT_ID
     ) >= 0
