1072a28bf6b45bf01af30afa29736b897d978674
Ivann LARUELLE <37900449+larueli@users.noreply.github.com>
2021-02-14 15:33:32 +0100

    add loadbalancing abilities

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 /**
  *
  *
@@ -36,6 +37,8 @@ Sentry.init({
   ["ROLE_VACANCES_ENSEIGNANT_ID", process.env.ROLE_VACANCES_ENSEIGNANT_ID],
   ["BOT_PREFIX", process.env.BOT_PREFIX],
   ["CATEGORY_AMPHI", process.env.CATEGORY_AMPHI],
+  ["WEB_LISTEN", process.env.WEB_LISTEN],
+  ["DISCORD_LISTEN", process.env.DISCORD_LISTEN]
 ].forEach(function (env) {
   if (typeof env[1] === "undefined" || env[1] === "") {
     console.error("La variable d'env " + env[0] + " n'est pas définie !");
@@ -82,32 +85,35 @@ client.on("ready", function () {
   ready(client);
 });
 
-/**
- * Quand un utilisateur rejoint le serveur, on lui envoie un message de bienvenue pour lui dire de se connecter au site etu
- */
-client.on("guildMemberAdd", (/** GuildMember */ member) => {
-  guildMemberAdd(member);
-});
-/** Si le bot reçoit un message en privé, ou sur l'un des channels qu'il peut voir */
-client.on("message", async (/** module:"discord.js".Message */ msg) => {
-  await message(
-    msg,
-    tableauChannelTexteAChannelVocal,
-    tableauChannelsVocauxEnCours
-  );
-});
+if(process.env.DISCORD_LISTEN === "1")
+{
+  /**
+   * Quand un utilisateur rejoint le serveur, on lui envoie un message de bienvenue pour lui dire de se connecter au site etu
+   */
+  client.on("guildMemberAdd", (/** GuildMember */ member) => {
+    guildMemberAdd(member);
+  });
+  /** Si le bot reçoit un message en privé, ou sur l'un des channels qu'il peut voir */
+  client.on("message", async (/** module:"discord.js".Message */ msg) => {
+    await message(
+      msg,
+      tableauChannelTexteAChannelVocal,
+      tableauChannelsVocauxEnCours
+    );
+  });
 
-client.on("voiceStateUpdate", async (
-  /** module:"discord.js".VoiceState */ oldState,
-  /** module:"discord.js".VoiceState */ newState
-) => {
-  await voiceStateUpdate(
-    oldState,
-    newState,
-    tableauChannelTexteAChannelVocal,
-    tableauChannelsVocauxEnCours
-  );
-});
+  client.on("voiceStateUpdate", async (
+    /** module:"discord.js".VoiceState */ oldState,
+    /** module:"discord.js".VoiceState */ newState
+  ) => {
+    await voiceStateUpdate(
+      oldState,
+      newState,
+      tableauChannelTexteAChannelVocal,
+      tableauChannelsVocauxEnCours
+    );
+  });
+}
 
 client.login(process.env.BOT_TOKEN).catch(console.error);
 
@@ -118,65 +124,67 @@ client.login(process.env.BOT_TOKEN).catch(console.error);
  *
  *
  *  */
-let createError = require("http-errors");
-let express = require("express");
-let path = require("path");
-let cookieParser = require("cookie-parser");
-let serveIndex = require("serve-index");
-let app = express();
+if(process.env.WEB_LISTEN === "1") {
+  let createError = require("http-errors");
+  let express = require("express");
+  let path = require("path");
+  let cookieParser = require("cookie-parser");
+  let serveIndex = require("serve-index");
+  let app = express();
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "twig");
+  app.set("views", path.join(__dirname, "views"));
+  app.set("view engine", "twig");
 
 //app.use(logger(process.env.APP));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, "public")));
 
-if (
-  typeof process.env.SITE_ETU_CLIENT_ID !== "undefined" &&
-  process.env.SITE_ETU_CLIENT_ID !== "" &&
-  typeof process.env.SITE_ETU_CLIENT_SECRET !== "undefined" &&
-  process.env.SITE_ETU_CLIENT_SECRET !== ""
-) {
-  app.use("/connexion", require("./routes/connexion"));
-  app.use("/attribuerrole", require("./routes/attribuerrole")(client));
-  app.use("/", require("./routes/home"));
-} else {
-  app.get("/", function (req, res) {
-    res.send(
-      "La connexion avec le site etu n'est pas possible en raison d'une mauvaise configuration du bot, ou alors <a href='https://etu.utt.fr'>le site etu</a> n'est pas accessible. Ressayez plus tard."
+  if (
+    typeof process.env.SITE_ETU_CLIENT_ID !== "undefined" &&
+    process.env.SITE_ETU_CLIENT_ID !== "" &&
+    typeof process.env.SITE_ETU_CLIENT_SECRET !== "undefined" &&
+    process.env.SITE_ETU_CLIENT_SECRET !== ""
+  ) {
+    app.use("/connexion", require("./routes/connexion"));
+    app.use("/attribuerrole", require("./routes/attribuerrole")(client));
+    app.use("/", require("./routes/home"));
+  } else {
+    app.get("/", function (req, res) {
+      res.send(
+        "La connexion avec le site etu n'est pas possible en raison d'une mauvaise configuration du bot, ou alors <a href='https://etu.utt.fr'>le site etu</a> n'est pas accessible. Ressayez plus tard."
+      );
+    });
+  }
+  if (
+    typeof process.env.DISCORD_CHAT_EXPORT_PATH !== "undefined" &&
+    typeof process.env.DISCORD_CHAT_EXPORTER_EXE_PATH !== "undefined" &&
+    process.env.DISCORD_CHAT_EXPORT_PATH !== "" &&
+    process.env.DISCORD_CHAT_EXPORTER_EXE_PATH !== ""
+  ) {
+    app.use(
+      "/exports",
+      express.static("public/exports"),
+      serveIndex("public/exports", { icons: true })
     );
-  });
-}
-if (
-  typeof process.env.DISCORD_CHAT_EXPORT_PATH !== "undefined" &&
-  typeof process.env.DISCORD_CHAT_EXPORTER_EXE_PATH !== "undefined" &&
-  process.env.DISCORD_CHAT_EXPORT_PATH !== "" &&
-  process.env.DISCORD_CHAT_EXPORTER_EXE_PATH !== ""
-) {
-  app.use(
-    "/exports",
-    express.static("public/exports"),
-    serveIndex("public/exports", { icons: true })
-  );
-}
+  }
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
 // error handler
-app.use(function (err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  app.use(function (err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  });
 
-module.exports = app;
+  module.exports = app;
+}
