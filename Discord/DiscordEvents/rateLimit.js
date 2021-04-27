84f2b64c65e85242222672d39229a2a65f82779f
Ivann LARUELLE <ivann+git@laruelle.me>
2021-04-28 00:40:42 +0200

    fix(rateLimit): show complete rateLimitInfo object in console

--- Discord/DiscordEvents/rateLimit.js
+++ Discord/DiscordEvents/rateLimit.js
@@ -4,4 +4,5 @@ module.exports = async function (
   console.log("!!!!!!!!!!!!!!!!! ALERTE !!!!!!!!!!!!!!!!!!");
   console.log("RateLimit atteint sur "+rateLimitInfo.route+" - "+rateLimitInfo.path+" avec "+rateLimitInfo.method);
   console.log("Timeout (ms) : "+ rateLimitInfo.timeout+" / Limite : "+rateLimitInfo.limit);
+  console.log(rateLimitInfo);
 };
