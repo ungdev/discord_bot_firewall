25ab8aa5a94fdbe801dc88f361bf8e4aeaefe0cb
Ivann LARUELLE <ivann+git@laruelle.me>
2021-04-28 12:17:28 +0200

    chore(cron): using await on sync

--- routes/cron.js
+++ routes/cron.js
@@ -8,6 +8,7 @@ module.exports = function (/** module:"discord.js".Client" */ client) {
 
   router.get("/cleanExports", function(req, res) {
     res.send("ok");
+    res.end();
     console.log("Nettoyage des exports !");
     let shell = require("shelljs");
     shell.exec(
@@ -17,8 +18,9 @@ module.exports = function (/** module:"discord.js".Client" */ client) {
     );
   });
 
-  router.get("/sync", function(req, res) {
+  router.get("/sync", async function(req, res) {
     res.send("Sync lancée !");
+    res.end();
 
     let donnees = {
       grant_type: "client_credentials",
@@ -27,40 +29,34 @@ module.exports = function (/** module:"discord.js".Client" */ client) {
       client_secret: process.env.SITE_ETU_CLIENT_SECRET,
     };
     let access_token = "";
-    axios
-      .post(utils.baseUrl + "/api/oauth/token?" + httpBuildQuery(donnees))
-      .then(function(request_token) {
-        access_token = request_token.data.access_token.toString();
-        /** Si autorisation ok, on affiche le formulaire de saisie de l'identifiant discord */
-        if (access_token !== "") {
-          let requete = { 'wantsJoinUTTDiscord': true, "access_token": access_token, "page": 1 };
+    let request_token = await axios.post(utils.baseUrl + "/api/oauth/token?" + httpBuildQuery(donnees));
+    access_token = request_token.data.access_token.toString();
+    if (access_token !== "") {
+      let requete = { 'wantsJoinUTTDiscord': true, "access_token": access_token, "page": 1 };
 
-          let otherPage = true;
-          let guild = client.guilds.cache.get(process.env.SERVER_ID);
-          let compteur = 0;
-          axios.get(utils.baseUrl + "/api/public/users?" + httpBuildQuery(requete)).then(async (page_request) => {
-            for (const i of Array(page_request.data.pagination.totalPages)) {
-              axios.get(utils.baseUrl + "/api/public/users?" + httpBuildQuery(requete)).then(async (response) => {
-                  otherPage = response.data.pagination.next ? true : false;
-                  requete.page = requete.page + 1;
-                  for (const etuUser of response.data.data) {
-                    if (etuUser.discordTag !== undefined && etuUser.discordTag !== "") {
-                      await assignFromWeb.etuToDiscord(
+        let otherPage = true;
+        let guild = client.guilds.cache.get(process.env.SERVER_ID);
+        let compteur = 0;
+        let page_request = await axios.get(utils.baseUrl + "/api/public/users?" + httpBuildQuery(requete));
+        for (const i of Array(page_request.data.pagination.totalPages)) {
+          let response = await axios.get(utils.baseUrl + "/api/public/users?" + httpBuildQuery(requete));
+          otherPage = response.data.pagination.next ? true : false;
+          requete.page = requete.page + 1;
+          for (const etuUser of response.data.data) {
+            if (etuUser.discordTag !== undefined && etuUser.discordTag !== "") {
+              await assignFromWeb.etuToDiscord(
                         etuUser,
                         etuUser.discordTag,
                         guild
                       ).catch(console.error);
-                      compteur += 1;
-                    }
-                  }
-                }
-              );
+              compteur += 1;
             }
-          }).then(() => console.log(compteur+" utilisateurs traités."));
-        } else {
-          console.error("Erreur cron vers le site etu !!")
+          }
         }
-      });
+        console.log(compteur+" utilisateurs traités.");
+      } else {
+        console.error("Erreur cron vers le site etu !!")
+      }
   });
 
   return router;
