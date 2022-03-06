1f314326120738c5817aff01b73163dc98365126
imperosol <56346771+imperosol@users.noreply.github.com>
2022-03-06 23:18:39 +0100

    Update addUE.js
    Refactor. Extraction de la fonction `createUeChannel` vers le module `discordUtils`

--- Discord/Commands/addUE.js
+++ Discord/Commands/addUE.js
@@ -1,58 +1,40 @@
-const discordUtils = require("../discordUtils");
+require("discord.js");
+const {ChannelTypes} = require("discord.js/typings/enums");
+const {createUeChannel} = require("../discordUtils");
+
 
 module.exports = async function addUE(
-  /** import("discord.js").Message */ msg,
-  /** Array<String> */ parametres
+    /** import("discord.js").Message */ msg,
+    /** Array<String> */ args
 ) {
-  /** S'il n'y a pas 3 paramètres dont la mention d'un role ni de ce qui doit être créé */
-  if (
-    parametres.length !== 5 ||
-    !msg.mentions.roles.first() ||
-    !["texte", "vocal", "lesdeux"].includes(parametres[4].toLowerCase())
-  ) {
-    msg
-      .reply(
-        ` :warning:  Erreur. La syntaxe est \`${process.env.BOT_PREFIX} addUE @RoleUE <categoryID> texte | vocal | lesDeux\`. La catégorie et le rôle doivent déjà exister.`
-      )
-      .catch(console.error);
-  } else {
-    /** Si tout va bien */
-    /** On crée le texte avec aucune permission pour @everyone et les permissions d'écrire pour le rôle concerné */
+    /** S'il n'y a pas 3 paramètres dont la mention d'un role ni de ce qui doit être créé */
     if (
-      parametres[4].toLowerCase() === "texte" ||
-      parametres[4].toLowerCase() === "lesdeux"
+        args.length !== 5 ||
+        !msg.mentions.roles.first() ||
+        !["texte", "vocal", "lesdeux"].includes(args[4].toLowerCase())
     ) {
-      msg.guild.channels
-        .create(msg.mentions.roles.first().name.toLowerCase(), {
-          parent: parametres[3]
-        })
-        .then((channel) => {
-          channel.permissionOverwrites.edit(msg.guild.roles.everyone, discordUtils.toutesPermissionsOverwrite(false));
-          channel.permissionOverwrites.edit(msg.mentions.roles.first().id, discordUtils.permissionsLireEcrireBasiquesOverwrite(true));
-          channel.send(
-            `Bonjour <@&${
-              msg.mentions.roles.first().id
-            }>, votre channel texte vient d'être créé !`
-          );
-        })
-        .catch(console.error);
+        msg.reply(
+            " :warning:  Erreur. La syntaxe est \`${process.env.BOT_PREFIX} addUE @RoleUE <categoryID> texte | vocal " +
+            "| lesDeux\`. La catégorie et le rôle doivent déjà exister."
+        ).catch(console.error);
+    } else {
+        /** Si tout va bien */
+        const channel_type = args[4].toLowerCase();
+        const channel_role = msg.mentions.roles.first();
+        const category = args[3];
+        const channel_name = msg.mentions.roles.first().name.toLowerCase();
+        /** On crée le texte avec aucune permission pour @everyone et les permissions d'écrire pour le rôle concerné */
+        if (["texte", "lesdeux"].includes(channel_type)) {
+            await createUeChannel(
+                msg.guild, channel_name, channel_role, category, ChannelTypes.GUILD_TEXT
+            );
+        }
+        /** On crée le vocal avec aucune permission pour @everyone et les permissions de parler/connecter pour le rôle concerné */
+        if (["vocal", "lesdeux"].includes(channel_type)) {
+            await createUeChannel(
+                msg.guild, channel_name, channel_role, category, ChannelTypes.GUILD_VOICE
+            );
+        }
+        msg.react('✅').catch(console.error);
     }
-    /** On crée le vocal avec aucune permission pour @everyone et les permissions de parler/connecter pour le rôle concerné */
-    if (
-      parametres[4].toLowerCase() === "vocal" ||
-      parametres[4].toLowerCase() === "lesdeux"
-    ) {
-      msg.guild.channels
-        .create(`${msg.mentions.roles.first().name.toLowerCase()} - vocal`, {
-          parent: parametres[3],
-          type: "GUILD_VOICE",
-          userLimit: 99
-        }).then((channel => {
-        channel.permissionOverwrites.edit(msg.guild.roles.everyone, discordUtils.toutesPermissionsOverwrite(false));
-        channel.permissionOverwrites.edit(msg.mentions.roles.first().id, discordUtils.permissionsLireEcrireBasiquesOverwrite(true));
-      }))
-        .catch(console.error);
-    }
-    msg.react('✅').catch(console.error);
-  }
 };
