7ef93b49dfac3799fb52bbf154dfe4007bf1bc68
imperosol <56346771+imperosol@users.noreply.github.com>
2022-03-07 12:55:15 +0100

    Update getRoles.js
    changé `\n\t-` par `, `
    Changé `args` par `parametres`

--- Discord/Commands/getRoles.js
+++ Discord/Commands/getRoles.js
@@ -1,18 +1,18 @@
 module.exports = async function getRoles(
     /** import("discord.js").Message */ msg,
-    /** Array<String> */ args
+    /** Array<String> */ parametres
 ) {
-    if (args.length !== 3) {
+    if (parametres.length !== 3) {
         msg.reply(
             ` :warning:  Erreur. La syntaxe est \`${process.env.BOT_PREFIX} getRoles NombreDePersonnes\`.`
         ).catch(console.error);
         return;
     }
-    const nbMembers = parseInt(args[2]);
+    const nbMembers = parseInt(parametres[2]);
     const roles = (await msg.guild.roles.fetch())
         .filter(role => role.members.size === nbMembers)
         .map(role => role.name);
     await msg.channel.send(
-        `:white_check_mark: ${roles.length} roles ont été identifiés : \n -` + roles.join("\n -")
+        `:white_check_mark: ${roles.length} roles ont été identifiés : \n -` + roles.join(", ")
     );
 };
