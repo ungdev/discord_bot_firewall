d213d76642908c20bc3cd542446fbcf9e3baaf85
thomas girod <56346771+imperosol@users.noreply.github.com>
2022-09-12 21:50:25 +0200

    Add files via upload
    ajout du fichier pour la commande certif

---
+++ Discord/Commands/certif.js
@@ -0,0 +1,17 @@
+/**
+ * Répond au message de la commande avec un message indiquant
+ * comment accéder à son certificat de scolarité
+ *
+ * @param msg : String
+ * @returns {Promise<void>}
+ */
+module.exports = async function send_certif(msg) {
+    msg.reply(
+        "Comment trouver son certificat de scolarité : \n" +
+        "1. Aller sur https://ent.utt.fr/ ;\n" +
+        "2. Se connecter avec ses identifiants UTT ;\n" +
+        "3. Suivre le chemin suivant : onglet \"Formation\" --> sous-onglet \"Dossier des étudiants\" ;\n" +
+        "4. Aller dans le menu \"Mes documents administratifs\" ;\n" +
+        "5. Récupérer le document souhaité."
+    );
+}
