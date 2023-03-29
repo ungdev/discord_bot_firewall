2c4392dd1de34ada5ec65d7d1d26c83cdb96efde
maréchal <thgirod@hotmail.com>
2023-03-30 00:13:54 +0200

    apply eslint

--- Discord/Commands/certif.js
+++ Discord/Commands/certif.js
@@ -2,16 +2,16 @@
  * Répond au message de la commande avec un message indiquant
  * comment accéder à son certificat de scolarité
  *
- * @param msg : String
+ * @param msg : import("discord.js").Message
  * @returns {Promise<void>}
  */
-module.exports = async function send_certif(msg) {
-    msg.reply(
-        "Comment trouver son certificat de scolarité : \n" +
-        "1. Aller sur https://ent.utt.fr/ ;\n" +
-        "2. Se connecter avec ses identifiants UTT ;\n" +
-        "3. Suivre le chemin suivant : onglet \"Formation\" --> sous-onglet \"Dossier des étudiants\" ;\n" +
-        "4. Aller dans le menu \"Mes documents administratifs\" ;\n" +
-        "5. Récupérer le document souhaité."
-    );
-}
+module.exports = async function certif(msg) {
+  await msg.reply(
+    "Comment trouver son certificat de scolarité : \n" +
+      "1. Aller sur https://ent.utt.fr/ ;\n" +
+      "2. Se connecter avec ses identifiants UTT ;\n" +
+      '3. Suivre le chemin suivant : onglet "Formation" --> sous-onglet "Dossier des étudiants" ;\n' +
+      '4. Aller dans le menu "Mes documents administratifs" ;\n' +
+      "5. Récupérer le document souhaité."
+  );
+};
